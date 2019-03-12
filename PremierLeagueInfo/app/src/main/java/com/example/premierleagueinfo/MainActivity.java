ab9f1d331980bf9bb02abd5595670796b291cdc9
X00126750 <X00126750@computing.stu.it-tallaght.ie>
2019-03-12 16:12:20 +0000

    Started android application that has dropdown menu of all premier league teams

---
+++ PremierLeagueInfo/app/src/main/java/com/example/premierleagueinfo/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.premierleagueinfo;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        TextView textView = (TextView)findViewById(R.id.simpleTextView);
+
+        textView.setText("Premier League Teams");
+
+        Spinner mySpinner = (Spinner) findViewById(R.id.SpinnerTeams);
+
+        ArrayAdapter<String> myAdaptor = new ArrayAdapter<String>(MainActivity.this,
+                android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.Teams));
+
+
+        myAdaptor.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                mySpinner.setAdapter(myAdaptor);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
