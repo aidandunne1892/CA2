7f88d3754992ddc7d8eb47ce0d5c2d94e81c3d53
X00126750 <X00126750@computing.stu.it-tallaght.ie>
2019-03-17 13:03:07 +0000

    Added button and add volley to the bulid gradle

--- PremierLeagueInfo/app/src/main/java/com/example/premierleagueinfo/MainActivity.java
+++ PremierLeagueInfo/app/src/main/java/com/example/premierleagueinfo/MainActivity.java
@@ -1,27 +1,26 @@
 package com.example.premierleagueinfo;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
-import android.widget.TextView;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
 
 public class MainActivity extends AppCompatActivity {
-
+String url = "www.something.com";
+    RequestQueue queue = Volley.newRequestQueue(this);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-       final TextView textView = findViewById(R.id.simpleTextView);
-        textView.setText(R.string.Premier_League_Team);
+
+
 
 
 
@@ -33,6 +32,9 @@ public class MainActivity extends AppCompatActivity {
 
         myAdaptor.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
                 mySpinner.setAdapter(myAdaptor);
+
+
+                Button sendButton = findViewById(R.id.SendToApi);
 
 
     }
